5f89b475083f9a379e0f1edeba50758dc2b9fcd1
Brian <bbannj@fastmail.fm>
2017-08-07 09:35:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,33 @@ $(document).ready(function() {
     $.getJSON(URL, {'api-key': KEY, 'begin_date': startMMDD, 'end_date': endMMDD, 'fq': 'glocations:('+state+')' }, 
     function(data) {
       console.log(data);
-      const results = data.response.docs.map((doc, index) => showResults(doc));
-      $('#results').html(results);
-    });
+      // if(data.response.meta.hits == 0) {
+      //   $('#results').hide();
+    
+      //   //alert("No results for this time");
+      //   return $('#no-results').text("Sorry, no results for this time and place.");
+        
+  
+      // }
+      if(data.response.meta.hits > 0){
+        $('#no-results').hide();
+        $('#results').show();
+        const results = data.response.docs.map((doc, index) => showResults(doc));
+        $('#results').html(results);
+      }
+      
+      else {
+        $('#results').hide();
+        $('#no-results').show();
+      
+        //alert("No results for this time");
+        return $('#no-results').text("Sorry, no results for this time and place.");
+      }
+
+  });
   }
   
+  //response.meta.hits == 0
   //Dates user sees = MM/YYYY OR August,1950
   //Dates API sees = YYYYMMDD to YYYYMMDD OR 19500801 to 19500832
   
@@ -85,13 +107,13 @@ $(document).ready(function() {
   
   function showResults(doc) {
     console.log(doc.multimedia[2]);
+    //$('#no-results').hide();
+    
     return `
         <li>
           <h3>${doc.headline.main}</h3>
-          <p>${doc.snippet}</p>
-     
-          <p>${doc.web_url}</p>
-          <p>${doc.source}</p>
+          <p class="snippet">${doc.snippet}</p>
+          <a href="${doc.web_url}" target="_blank" class="link">Read more at nytimes.com</a>
         </li>
       `;
   }
@@ -99,6 +121,8 @@ $(document).ready(function() {
   
   $('#search-form').submit(function(event) {
     event.preventDefault();
+   $('#results-col').show();
+    
     var month = $('#month').val();
     //var year = $('#year').val();
     var state = $('#state').val();
@@ -111,7 +135,6 @@ $(document).ready(function() {
 //location problems
 //multimedia
 //<img src="http://nytimes.com/${doc.multimedia[2].url}" />
-
 
 
 
